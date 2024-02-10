91a5cdf8acca2465dc513b39c59e4e5ca3437995
박순형 <60930113+tnsgud@users.noreply.github.com>
2024-02-11 01:04:02 +0900

    add css

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,5 @@
-import Link from 'next/link';
+import Movie from '../../components/movie';
+import styles from '../../styles/home.module.css';
 
 export const metadata = {
   title: 'Home',
@@ -17,11 +18,14 @@ export default async function HomePage() {
   const movies = await getMovies();
 
   return (
-    <div>
-      {...movies.map((movie) => (
-        <li>
-          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
-        </li>
+    <div className={styles.container}>
+      {movies.map((movie) => (
+        <Movie
+          key={movie.id}
+          id={movie.id}
+          title={movie.title}
+          poster_path={movie.poster_path}
+        />
       ))}
     </div>
   );
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,9 +1,7 @@
+import styles from '../styles/movie-videos.module.css';
 import { API_URL } from '../app/(home)/page';
 
 async function getVideos(id: string) {
-  console.log(`Fetching videos: ${Date.now()}`);
-  // await new Promise((resolve) => setTimeout(resolve, 3000));
-  // throw new Error('something broke....');
   const response = await fetch(`${API_URL}/${id}/videos`);
 
   return response.json();
@@ -12,5 +10,16 @@ async function getVideos(id: string) {
 export default async function ({ id }: { id: string }) {
   const videos = await getVideos(id);
 
-  return <h6>{JSON.stringify(videos)}</h6>;
+  return (
+    <div className={styles.container}>
+      {videos.map((video) => (
+        <iframe
+          key={video.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          title={video.name}
+          allowFullScreen
+        />
+      ))}
+    </div>
+  );
 }
