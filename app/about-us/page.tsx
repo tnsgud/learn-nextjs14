d9843c70d6b0915a346224922c4678fd3863d7dd
박순형 <60930113+tnsgud@users.noreply.github.com>
2024-02-11 00:13:50 +0900

    init

---
+++ app/about-us/page.tsx
@@ -0,0 +1,9 @@
+import Navigation from '../../components/naviagation';
+
+export default function AboutUs() {
+  return (
+    <div>
+      <h1>About Us</h1>
+    </div>
+  );
+}
---
+++ components/movie-videos.tsx
@@ -0,0 +1,16 @@
+import { API_URL } from '../app/(home)/page';
+
+async function getVideos(id: string) {
+  console.log(`Fetching videos: ${Date.now()}`);
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
+  // throw new Error('something broke....');
+  const response = await fetch(`${API_URL}/${id}/videos`);
+
+  return response.json();
+}
+
+export default async function ({ id }: { id: string }) {
+  const videos = await getVideos(id);
+
+  return <h6>{JSON.stringify(videos)}</h6>;
+}
