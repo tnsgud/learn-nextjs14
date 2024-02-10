3637c1015dc91259a17c86c921626ce4a7f7963b
박순형 <60930113+tnsgud@users.noreply.github.com>
2024-02-11 01:11:30 +0900

    dynamic metadata

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,7 +1,7 @@
 import styles from '../styles/movie-info.module.css';
 import { API_URL } from '../app/(home)/page';
 
-async function getMovie(id: string) {
+export async function getMovie(id: string) {
   const response = await fetch(`${API_URL}/${id}`);
 
   return response.json();
